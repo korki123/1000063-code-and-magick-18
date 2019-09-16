453619af309b6262a3bda00c75b8b97ee9b1e75e
Игорь Хан <korki123@mail.ru>
2019-09-16 17:18:01 +0300

    Почистил магию

--- js/stat.js
+++ js/stat.js
@@ -7,9 +7,18 @@ var AREA_Y = 10;
 var AREA_SHADOW_X = 110;
 var AREA_SHADOW_Y = 20;
 var COLUMN_WIDTH = 40;
+var MAX_COLUMN_HEIGHT = 150;
+var LOCATION_X = 150;
+var LOCATION_Y_WINNER = 20;
+var LOCATION_Y_RESULT = 35;
+var LOCATION_Y_GAMERS_ROW = 255;
 var GAP = 50;
+var OFFSET_Y_GAMERS_TIMES = 70;
 var TEXT_FONTS = '14px PT Mono';
 var COLOR_BLACK = 'rgb(0, 0, 0)';
+var COLOR_WHITE = 'rgb(255, 255, 255)';
+var MAIN_COLOR = 'rgba(255, 0, 0, 1)';
+var COLOR_SHADOW = 'rgba(0, 0, 0, 0.7)';
 
 var getMaxTimes = function (times) {
   var maxTime = 0;
@@ -34,35 +43,35 @@ var paintRandom = function (names) {
 
 window.renderStatistics = function (ctx, names, times) {
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillStyle = COLOR_SHADOW;
   ctx.fillRect(AREA_SHADOW_X, AREA_SHADOW_Y, AREA_WIDTH, AREA_HEIGHT);
 
-  ctx.fillStyle = 'rgb(255, 255, 255)';
+  ctx.fillStyle = COLOR_WHITE;
   ctx.fillRect(AREA_X, AREA_Y, AREA_WIDTH, AREA_HEIGHT);
 
   ctx.fillStyle = COLOR_BLACK;
   ctx.font = TEXT_FONTS;
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 150, 20);
-  ctx.fillText('Список результатов:', 150, 35);
+  ctx.fillText('Ура вы победили!', LOCATION_X, LOCATION_Y_WINNER);
+  ctx.fillText('Список результатов:', LOCATION_X, LOCATION_Y_RESULT);
 
   for (var i = 0; i < times.length; i++) {
 
-    var colorColumn = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : paintRandom(names);
+    var colorColumn = names[i] === 'Вы' ? MAIN_COLOR : paintRandom(names);
 
-    var columnHeight = 150 / getMaxTimes(times);
+    var columnHeight = MAX_COLUMN_HEIGHT / getMaxTimes(times);
 
     ctx.fillStyle = colorColumn;
-    ctx.fillRect(150 + i * (COLUMN_WIDTH + GAP), 90 + (150 - columnHeight * times[i]), COLUMN_WIDTH, columnHeight * times[i]);
+    ctx.fillRect(LOCATION_X + i * (COLUMN_WIDTH + GAP), COLUMN_WIDTH + GAP + (MAX_COLUMN_HEIGHT - columnHeight * times[i]), COLUMN_WIDTH, columnHeight * times[i]);
 
-    var columnNames = 150 + i * (COLUMN_WIDTH + GAP);
+    var columnNames = LOCATION_X + i * (COLUMN_WIDTH + GAP);
     ctx.fillStyle = COLOR_BLACK;
     ctx.font = TEXT_FONTS;
     ctx.textBaseline = 'hanging';
-    ctx.fillText(names[i], columnNames, 255);
+    ctx.fillText(names[i], columnNames, LOCATION_Y_GAMERS_ROW);
 
-    var gameTimes = 70 + (150 - columnHeight * times[i]);
-    ctx.fillStyle = 'rgb(0, 0, 0)';
+    var gameTimes = OFFSET_Y_GAMERS_TIMES + (LOCATION_X - columnHeight * times[i]);
+    ctx.fillStyle = COLOR_BLACK;
     ctx.font = TEXT_FONTS;
     ctx.fillText(times[i], columnNames, gameTimes);
   }
