ea99c351fec9b442ceec1721aaa69f3cbd434dc5
Игорь Хан <korki123@mail.ru>
2019-09-19 10:52:00 +0300

    Упростил

--- js/setup.js
+++ js/setup.js
@@ -10,62 +10,88 @@ var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var numberOfPlayers = 4;
 
 var selectedWizardNames = function () {
-  var i = numberOfPlayers;
-  i <
+  // var i = numberOfPlayers;
+  // i > WIZARD_NAMES.length ? i = i : i = WIZARD_NAMES.length;
+  // i > WIZARD_SURNAMES.length ? i = i : i = WIZARD_SURNAMES.length;
+  // i > WISARD_COAT_COLORS.length ? i = i : i = WISARD_COAT_COLORS.length;
+  // i > WIZARD_EYES_COLOR.length ? i = i : i = WIZARD_EYES_COLOR.length;
 
 
   for (WIZARD_NAMES.length; WIZARD_NAMES.length > numberOfPlayers; WIZARD_NAMES.length += 0) {
 
-    var rand = Math.floor(Math.random() * WIZARD_NAMES.length);
-    WIZARD_NAMES.splice(rand, 1);
+    var randNames = Math.floor(Math.random() * WIZARD_NAMES.length);
+    WIZARD_NAMES.splice(randNames, 1);
+
+    var randSurnames = Math.floor(Math.random() * WIZARD_SURNAMES.length);
+    WIZARD_SURNAMES.splice(randSurnames, 1);
+
+    var randCoatColors = Math.floor(Math.random() * WISARD_COAT_COLORS.length);
+    WISARD_COAT_COLORS.splice(randCoatColors, 1);
+
+    var randEyesColor = Math.floor(Math.random() * WIZARD_EYES_COLOR.length);
+    WIZARD_EYES_COLOR.splice(randEyesColor, 1);
   }
-  return WIZARD_NAMES;
+
+  console.log('Имя ' + WIZARD_NAMES + 'фамилия ' + randSurnames);
+
+  for (var i = 0; i < numberOfPlayers; i++) {
+    var wizardReady = [
+      wizardReady[i] = {
+        name: WIZARD_NAMES[i],
+        surname: WIZARD_SURNAMES[i],
+        coatColor: WISARD_COAT_COLORS[i],
+        eyesColor: WIZARD_EYES_COLOR[i],
+      }
+    ];
+  }
+  return wizardReady;
+  console.log(wizardReady);
 };
+
 console.log(selectedWizardNames());
 
-var selectedWizardNamesSurnames = function () {
+// var selectedWizardNamesSurnames = function () {
 
-  for (WIZARD_SURNAMES.length; WIZARD_SURNAMES.length > 4; WIZARD_SURNAMES.length += 0) {
+  // for (WIZARD_SURNAMES.length; WIZARD_SURNAMES.length > numberOfPlayers; WIZARD_SURNAMES.length += 0) {
 
-    var rand = Math.floor(Math.random() * WIZARD_SURNAMES.length);
-    WIZARD_SURNAMES.splice(rand, 1);
-  }
+    // var rand = Math.floor(Math.random() * WIZARD_SURNAMES.length);
+    // WIZARD_SURNAMES.splice(rand, 1);
+  // }
 
-  return WIZARD_SURNAMES;
-};
-console.log(selectedWizardNamesSurnames());
-
-
-var selectedWisardCoatColors = function () {
-
-  for (WISARD_COAT_COLORS.length; WISARD_COAT_COLORS.length > 4; WISARD_COAT_COLORS.length += 0) {
-
-    var rand = Math.floor(Math.random() * WISARD_COAT_COLORS.length);
-    WISARD_COAT_COLORS.splice(rand, 1);
-  }
-
-  return WISARD_COAT_COLORS;
-};
-console.log(selectedWisardCoatColors());
-
-var selectedWizardEyesColor = function () {
-
-  for (WIZARD_EYES_COLOR.length; WIZARD_EYES_COLOR.length > 4; WIZARD_EYES_COLOR.length += 0) {
-
-    var rand = Math.floor(Math.random() * WIZARD_EYES_COLOR.length);
-    WIZARD_EYES_COLOR.splice(rand, 1);
-  }
-
-  return WIZARD_EYES_COLOR;
-};
-console.log(selectedWizardEyesColor());
-
-var wizard = [];
-
-for (var i = 0; i < WIZARD_NAMES.length; i++) {
-  wizard
-
-}
+//   return WIZARD_SURNAMES;
+// };
+// console.log(selectedWizardNamesSurnames());
 
 
-console.log(wizard);
+// var selectedWisardCoatColors = function () {
+
+  // for (WISARD_COAT_COLORS.length; WISARD_COAT_COLORS.length > numberOfPlayers; WISARD_COAT_COLORS.length += 0) {
+
+    // var rand = Math.floor(Math.random() * WISARD_COAT_COLORS.length);
+    // WISARD_COAT_COLORS.splice(rand, 1);
+  // }
+
+//   return WISARD_COAT_COLORS;
+// };
+// console.log(selectedWisardCoatColors());
+
+// var selectedWizardEyesColor = function () {
+//
+//   for (WIZARD_EYES_COLOR.length; WIZARD_EYES_COLOR.length > numberOfPlayers; WIZARD_EYES_COLOR.length += 0) {
+
+    // var rand = Math.floor(Math.random() * WIZARD_EYES_COLOR.length);
+    // WIZARD_EYES_COLOR.splice(rand, 1);
+    // }
+
+//   return WIZARD_EYES_COLOR;
+// };
+// console.log(selectedWizardEyesColor());
+
+// var wizard = [];
+//
+// for (var i = 0; i < WIZARD_NAMES.length; i++) {
+//   wizard
+//
+// }
+
+// console.log(wizard);
