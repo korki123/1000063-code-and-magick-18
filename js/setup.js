28eb5c6f7aa4ad35842ea8729c6577a7285db20c
Игорь Хан <korki123@mail.ru>
2019-09-20 09:36:05 +0300

    Переписал код как в общаке

--- js/setup.js
+++ js/setup.js
@@ -8,44 +8,22 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var WISARD_COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var numberOfPlayers = 4;
+var NUMBER_OF_PLAYERS = 4;
 
-var selectedWizardNames = function () {
-
-  for (var i = 0; i < numberOfPlayers; i++) {
-
-    var randNames = Math.floor(Math.random() * WIZARD_NAMES.length);
-    console.log(WIZARD_NAMES[randNames]);
-
-    var randSurnames = Math.floor(Math.random() * WIZARD_SURNAMES.length);
-    console.log(WIZARD_SURNAMES[randSurnames]);
-
-    var randCoatColors = Math.floor(Math.random() * WISARD_COAT_COLORS.length);
-    console.log(WISARD_COAT_COLORS[randCoatColors]);
-
-    var randEyesColor = Math.floor(Math.random() * WIZARD_EYES_COLOR.length);
-    console.log(WIZARD_EYES_COLOR[randEyesColor]);
-
-    var wizardReady = [];
-
-    for (var j = 0; j < numberOfPlayers; j++) {
-      wizardReady.push({
-        name: WIZARD_NAMES[j],
-        surname: WIZARD_SURNAMES[j],
-        coat: WISARD_COAT_COLORS[j],
-        eye: WIZARD_EYES_COLOR[j],
-      });
-    }
-
-    WIZARD_NAMES.splice(randNames, 1);
-    WIZARD_SURNAMES.splice(randSurnames, 1);
-    WISARD_COAT_COLORS.splice(randCoatColors, 1);
-    WIZARD_EYES_COLOR.splice(randEyesColor, 1);
-  }
-
-  return wizardReady;
-  console.log(wizardReady);
-
+var selectedWizard = function (random) {
+  var randNames = Math.floor (Math.random() * random.length);
+  return random [randNames];
+  random.splice (random, 1);
 };
 
-console.log(selectedWizardNames());
+var wizardReady = [];
+
+for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
+  wizardReady.push ({
+    name: selectedWizard(WIZARD_NAMES),
+    surname: selectedWizard(WIZARD_SURNAMES),
+    coat: selectedWizard(WISARD_COAT_COLORS),
+    eye: selectedWizard(WIZARD_EYES_COLOR),
+  });
+}
+console.log(wizardReady);
