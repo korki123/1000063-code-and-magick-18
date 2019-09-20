4623cf57605c800dcb1cba000ba3964d328f0c62
Игорь Хан <korki123@mail.ru>
2019-09-20 19:40:30 +0300

    Третий пункт почти готов. Не красятся плащи

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var showSetup = document.querySelector('.setup');
-showSetup.classList.remove('hidden');
+var SHOW_SETUP = document.querySelector('.setup');
+SHOW_SETUP.classList.remove('hidden');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
@@ -11,15 +11,14 @@ var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var NUMBER_OF_PLAYERS = 4;
 
 var selectedWizard = function (random) {
-  var randNames = Math.floor (Math.random() * random.length);
-  return random [randNames];
-  random.splice (random, 1);
+  var randNames = Math.floor(Math.random() * random.length);
+  return random.splice(randNames, 1);
 };
 
 var wizardReady = [];
 
 for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
-  wizardReady.push ({
+  wizardReady.push({
     name: selectedWizard(WIZARD_NAMES),
     surname: selectedWizard(WIZARD_SURNAMES),
     coat: selectedWizard(WISARD_COAT_COLORS),
@@ -27,3 +26,21 @@ for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
   });
 }
 console.log(wizardReady);
+
+var SETUP_SIMILAR = document.querySelector('.setup-similar');
+
+SETUP_SIMILAR.classList.remove('hidden');
+
+var SETUP_SIMILAR_LIST = document.querySelector('.setup-similar-list');
+var SETUP_SIMILAR_ITEM = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+for (var j = 0; j < NUMBER_OF_PLAYERS; j++) {
+  var vizardItem = SETUP_SIMILAR_ITEM.cloneNode(true);
+
+  vizardItem.querySelector('.wizard-coat').style.fill = wizardReady[j].coat;
+  vizardItem.querySelector('.wizard-head').style.fill = wizardReady[j].coat;
+  vizardItem.querySelector('.wizard-eyes').style.fill = wizardReady[j].eye;
+  vizardItem.querySelector('.setup-similar-label').textContent = wizardReady[j].name + wizardReady[j].surname;
+
+  SETUP_SIMILAR_LIST.appendChild(vizardItem);
+}
