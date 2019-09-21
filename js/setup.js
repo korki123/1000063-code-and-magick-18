d5bd00af7efc300a93c354977a0fab7adad9a0b9
korki123 <korki123@mail.ru>
2019-09-21 12:18:07 +0300

    Покрасил плащи

--- js/setup.js
+++ js/setup.js
@@ -5,7 +5,7 @@ SHOW_SETUP.classList.remove('hidden');
 
 var WIZARD_NAMES = ['Иван ', 'Хуан Себастьян ', 'Мария ', 'Кристоф ', 'Виктор ', 'Юлия ', 'Люпита ', 'Вашингтон '];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var WISARD_COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var NUMBER_OF_PLAYERS = 4;
@@ -21,7 +21,7 @@ for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
   wizardReady.push({
     name: selectedWizard(WIZARD_NAMES),
     surname: selectedWizard(WIZARD_SURNAMES),
-    coat: selectedWizard(WISARD_COAT_COLORS),
+    coat: selectedWizard(WIZARD_COAT_COLORS),
     eye: selectedWizard(WIZARD_EYES_COLOR),
   });
 }
