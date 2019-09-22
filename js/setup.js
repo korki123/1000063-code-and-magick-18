f3e391b784f60b09b2159311ec417762d2cc98d4
Игорь Хан <korki123@mail.ru>
2019-09-22 14:27:47 +0300

    Переименовал рандомную функцию

--- js/setup.js
+++ js/setup.js
@@ -9,39 +9,38 @@ var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 1
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var NUMBER_OF_PLAYERS = 4;
-var SETUP_SIMILAR = document.querySelector('.setup-similar');
-var SETUP_SIMILAR_LIST = document.querySelector('.setup-similar-list');
-var SETUP_SIMILAR_ITEM = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-
-var selectedWizard = function (random) {
-  var randNames = Math.floor(Math.random() * random.length);
-  return random.splice(randNames, 1);
+var getRandomNames = function (random) {
+  var randomNames = Math.floor(Math.random() * random.length);
+  return random.splice(randomNames, 1);
 };
 
 var wizardReady = [];
 
 for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
   wizardReady.push({
-    name: selectedWizard(WIZARD_NAMES),
-    surname: selectedWizard(WIZARD_SURNAMES),
-    coat: selectedWizard(WIZARD_COAT_COLORS),
-    eye: selectedWizard(WIZARD_EYES_COLOR),
+    name: getRandomNames(WIZARD_NAMES),
+    surname: getRandomNames(WIZARD_SURNAMES),
+    coat: getRandomNames(WIZARD_COAT_COLORS),
+    eye: getRandomNames(WIZARD_EYES_COLOR),
   });
 }
 // console.log(wizardReady);
-var fragment = document.createDocumentFragment();
+
+var SETUP_SIMILAR = document.querySelector('.setup-similar');
+
+SETUP_SIMILAR.classList.remove('hidden');
+
+var SETUP_SIMILAR_LIST = document.querySelector('.setup-similar-list');
+var SETUP_SIMILAR_ITEM = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
 for (var j = 0; j < NUMBER_OF_PLAYERS; j++) {
   var vizardItem = SETUP_SIMILAR_ITEM.cloneNode(true);
 
   vizardItem.querySelector('.wizard-coat').style.fill = wizardReady[j].coat;
+  vizardItem.querySelector('.wizard-head').style.fill = wizardReady[j].coat;
   vizardItem.querySelector('.wizard-eyes').style.fill = wizardReady[j].eye;
   vizardItem.querySelector('.setup-similar-label').textContent = wizardReady[j].name + wizardReady[j].surname;
 
-  fragment.appendChild(vizardItem);
+  SETUP_SIMILAR_LIST.appendChild(vizardItem);
 }
-
-SETUP_SIMILAR_LIST.appendChild(fragment);
-
-SETUP_SIMILAR.classList.remove('hidden');
