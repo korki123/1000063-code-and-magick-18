2668db6d67e6fee79ab5c6717c0d67baad2b0dd4
korki123 <korki123@mail.ru>
2019-09-23 08:39:33 +0300

    Внес правки. Переименовал переменные

--- js/setup.js
+++ js/setup.js
@@ -11,20 +11,24 @@ var SETUP_SIMILAR = document.querySelector('.setup-similar');
 var SETUP_SIMILAR_LIST = document.querySelector('.setup-similar-list');
 var SETUP_SIMILAR_ITEM = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-var getRandomNames = function (random) {
+var getRandomItem = function (random) {
   var randomNumber = Math.floor(Math.random() * random.length);
   return random.splice(randomNumber, 1);
 };
 
 var prepareWizardsList = function () {
   var wizards = [];
+  var name = WIZARD_NAMES;
+  var surnames = WIZARD_SURNAMES;
+  var coatColor = WIZARD_COAT_COLORS;
+  var eyesColor = WIZARD_EYES_COLOR;
 
   for (var i = 0; i < NUMBER_OF_PLAYERS; i++) {
     wizards.push({
-      name: getRandomNames(WIZARD_NAMES),
-      surname: getRandomNames(WIZARD_SURNAMES),
-      coat: getRandomNames(WIZARD_COAT_COLORS),
-      eye: getRandomNames(WIZARD_EYES_COLOR),
+      name: getRandomItem(name),
+      surname: getRandomItem(surnames),
+      coat: getRandomItem(coatColor),
+      eye: getRandomItem(eyesColor),
     });
   }
   return wizards;
@@ -33,25 +37,23 @@ var prepareWizardsList = function () {
 var renderWizards = function () {
 
   var fragment = document.createDocumentFragment();
-  var wizardsCollect = prepareWizardsList();
+  var wizards = prepareWizardsList();
 
   for (var j = 0; j < NUMBER_OF_PLAYERS; j++) {
-    var vizardItem = SETUP_SIMILAR_ITEM.cloneNode(true);
+    var element = SETUP_SIMILAR_ITEM.cloneNode(true);
 
-    vizardItem.querySelector('.wizard-coat').style.fill = wizardsCollect[j].coat;
-    vizardItem.querySelector('.wizard-head').style.fill = wizardsCollect[j].coat;
-    vizardItem.querySelector('.wizard-eyes').style.fill = wizardsCollect[j].eye;
-    vizardItem.querySelector('.setup-similar-label').textContent = wizardsCollect[j].name + wizardsCollect[j].surname;
+    element.querySelector('.wizard-coat').style.fill = wizards[j].coat;
+    element.querySelector('.wizard-head').style.fill = wizards[j].coat;
+    element.querySelector('.wizard-eyes').style.fill = wizards[j].eye;
+    element.querySelector('.setup-similar-label').textContent = wizards[j].name + wizards[j].surname;
 
-    fragment.appendChild(vizardItem);
+    fragment.appendChild(element);
   }
 
   SETUP_SIMILAR_LIST.appendChild(fragment);
 
   SHOW_SETUP.classList.remove('hidden');
   SETUP_SIMILAR.classList.remove('hidden');
-
-  return SETUP_SIMILAR;
 };
 
 renderWizards();
